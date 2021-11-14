5e2973bea6897f50e392dfabfaef5899907e8a65
Ravishka Dulshan <92871474+Ravishka2000@users.noreply.github.com>
2021-11-14 12:41:23 +0000

    pause

---
+++ Past Papers/2017 (JUNE) PP/Question 1.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <string.h>
+#include <assert.h>
+
+
+void inputProduction(int array[],int size);
+int minimumDay(int array[],int size);
+int maximumDay(int array[],int size);
+int totalProduction(int array[],int size);
+float averageProduction(int array[], int size);
+void compareProduction(int array1[],int array2[],int array3[],int size);
+void displayProduction(int array[],int size,int day);
+
+
+
+int main()
+{
+
+
+
+  return 0;
+}
+
+
+
+void inputProduction(int array[],int size)
+{
+  int i;
+
+  for(i=0;i<7;++i)
+  {
+    printf("Enter Production of day %d : ",i+1);
+    scanf("%d",&array[i]);
+  }
+}
+
+int minimumDay(int array[],int size)
+{
+
+}
+
+int maximumDay(int array[],int size)
+{
+
+}
+
+int totalProduction(int array[],int size)
+{
+
+}
+
+float averageProduction(int array[], int size)
+{
+
+}
+
+void compareProduction(int array1[],int array2[],int array3[],int size)
+{
+
+}
+
+void displayProduction(int array[],int size,int day)
+{
+
+}
